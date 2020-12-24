09ffade4d1fae4764eb7a8264236c1cf606b6bb8
3240301324@qq.com <3240301324@qq.com>
2020-12-24 11:16:21 +0800

    222

--- WebApplication3/Controllers/HomeController.cs
+++ WebApplication3/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace WebApplication3.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult aaaaaa()
         {
             return View();
         }
